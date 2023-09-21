74a5963c766a9bc9af73522b936c7cf81ecb9491
Fidelis Wairimu <fidelis.wairimu@acquiretek.com>
2023-09-21 16:03:32 +0300

    Nested Components, services and DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConevertToSpacePipe } from './shared/convert-to-space.pipe';
+import { starComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConevertToSpacePipe,
+    starComponent
   ],
   imports: [
     BrowserModule,
